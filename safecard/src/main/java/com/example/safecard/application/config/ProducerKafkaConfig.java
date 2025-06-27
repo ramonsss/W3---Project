bcb3315921044a2ab8112d9f60f5be1892a2d417
ramonsss <momon14souza@gmail.com>
2025-06-27 18:15:44 -0300

    commitandozinho

---
+++ safecard/src/main/java/com/example/safecard/application/config/ProducerKafkaConfig.java
@@ -0,0 +1,30 @@
+package com.example.safecard.application.config;
+
+import java.util.HashMap;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+
+@Configuration
+public class ProducerKafkaConfig {
+    
+    @Autowired
+    private KafkaProperties kafkaProperties;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        var configs = new HashMap<String, Object>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+
+}
